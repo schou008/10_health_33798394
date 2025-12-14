c45b02a72784db2074af6032bdbc5314a9528f6c
schou008​ <saqibchy9@gmail.com>
2025-12-14 02:32:47 +0000

    Final Version

--- routes/admin.js
+++ routes/admin.js
@@ -96,7 +96,7 @@ router.get('/classes/delete/:id', adminOnly, (req, res, next) => {
 //Logout
 router.get('/logout', (req, res) => {
     req.session.admin = null;
-    res.redirect('admin/login');
+    res.redirect('./login');
 });
 
 module.exports = router;
